bc26282f8bd0edf1435c2baa67f17e68ecaecba8
Repository Orchestrator <maskom_team@ma-malnukananga.sch.id>
2025-11-21 00:56:38 +0000

    fix: add missing imports for RequestListener and DataSource in ImageLoader

--- app/src/main/java/com/example/iurankomplek/utils/ImageLoader.kt
+++ app/src/main/java/com/example/iurankomplek/utils/ImageLoader.kt
@@ -1,10 +1,15 @@
 package com.example.iurankomplek.utils
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import java.net.URL
 import com.example.iurankomplek.R
